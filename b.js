87a1de5f3d5df8109301b7cb54f313593c00c05e
jeffrey-hue-011 <jeffrey.doff@gmail.com>
2025-12-21 00:57:49 +0800

    adjust folders

---
+++ b.js
@@ -0,0 +1,11 @@
+const aaplDailyOHLC = [
+	[75.62, 74.14, 78.52, 80.21],
+	[80.21, 81.21, 79.89, 80.78]
+];
+for(let i = 0; i < aaplDailyOHLC.length; i++) {
+	console.log("Inner Array: ", aaplDailyOHLC[i])
+	console.log("Open: ",        aaplDailyOHLC[i][0]) // We are using index numbers to access items in each nested array
+	console.log("High: ",        aaplDailyOHLC[i][1])
+	console.log("Low: ",         aaplDailyOHLC[i][2])
+	console.log("Close: ",       aaplDailyOHLC[i][3])
+}
